0b5b3dc12d46dc06f9ca76b0509d306f12f03ac2
Anna Schulze <annasvea04@gmail.com>
2026-02-25 14:51:56 +0100

    clean up venv folders and update .gitignore

--- src/loading_cleaning_describing.py
+++ src/loading_cleaning_describing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 pd.set_option("display.max_columns", None)
+# added because df.info() didn't display all columns before
 
 df = pd.read_csv("data/raw/Data_Set_S1.txt", 
     sep="\t",
@@ -10,3 +11,7 @@ df = pd.read_csv("data/raw/Data_Set_S1.txt",
 
 df.info()
 print(df.head())
+# no missing values for word, happiness_rank, _average & _standard_deviation
+# 5222 missing values for all _rank
+# all numeric columns are already numeric types (_rank is int64, rest is float64)
+# 8 columns, 1022 rows
