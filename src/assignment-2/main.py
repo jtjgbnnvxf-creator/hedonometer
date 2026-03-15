fcc4d52951cc0ebf7540fae64dd00fd3fd0674b0
Sonya <vsofya6361@gmail.com>
2026-03-15 16:21:18 +0100

    fix: orchestration

--- src/assignment-2/main.py
+++ src/assignment-2/main.py
@@ -7,17 +7,18 @@ from datacleaning import clean_and_sample
 
 # 1. IMPORT THE DATASET
 df_business = pd.read_json(
-    'data/raw/assignment-2/yelp_academic_dataset_review.json',
+    'data/raw/assignment-2/yelp_sample.json',
     lines=True
 )
 
 # 2. DATASET CLEANING
-clean_df = clean_and_sample(df_business, sample_size=25) # change the sample size according to research logic
+clean_df = clean_and_sample(df_business, sample_size=250000) # change the sample size according to research logic
 
 # 3. SCORING
-
+pass
 
 # 4. ANALYSIS
-
+pass
 
 # 5. VISUALIZATION
+pass
